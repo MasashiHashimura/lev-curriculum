2fc29217ade706a8f89d80be28dd699b28f7756f
MasashiHashimura <youzhiqiaocun303@gmail.com>
2022-09-14 12:24:13 +0900

    finishcurriculum9-3

--- blog/resources/views/categories/index.blade.php
+++ blog/resources/views/categories/index.blade.php
@@ -7,32 +7,35 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-
-        <p class='create'>[<a href='/posts/create'>create</a>]</p>
-
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                    <form action="/posts/{{ $post->id }}" id="form_delete" method="post" style="display:inline">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" style="display:none;"></input>
-                        <button type="submit">[<span onclick="return deletePost(this);">delete</span>]</button> 
-                    </form>
-                </div>
-            @endforeach
-        </div>
-                <div class="footer">
-            <a href="/">back</a>
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+        @extends('layouts.app')
+        @section('content')
+            <h1>Blog Name</h1>
+    
+            <p class='create'>[<a href='/posts/create'>create</a>]</p>
+    
+            <div class='posts'>
+                @foreach ($posts as $post)
+                    <div class='post'>
+                        <h2 class='title'>
+                            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h2>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                        <p class='body'>{{ $post->body }}</p>
+                        <form action="/posts/{{ $post->id }}" id="form_delete" method="post" style="display:inline">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" style="display:none;"></input>
+                            <button type="submit">[<span onclick="return deletePost(this);">delete</span>]</button> 
+                        </form>
+                    </div>
+                @endforeach
+            </div>
+                    <div class="footer">
+                <a href="/">back</a>
+            </div>
+            <div class='paginate'>
+                {{ $posts->links() }}
+            </div>
+        @endsection
     </body>
 </html>
--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -7,23 +7,29 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>Blog Name</h1>
-
-        <p class='create'>[<a href='/posts/create'>create</a>]</p>
-
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2 class='title'>
-                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                    </h2>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+        @extends('layouts.app')
+        @section('content')
+        
+        {{Auth::user()->name}}
+        
+            <h1>Blog Name</h1>
+    
+            <p class='create'>[<a href='/posts/create'>create</a>]</p>
+    
+            <div class='posts'>
+                @foreach ($posts as $post)
+                    <div class='post'>
+                        <h2 class='title'>
+                            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h2>
+                        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                        <p class='body'>{{ $post->body }}</p>
+                    </div>
+                @endforeach
+            </div>
+            <div class='paginate'>
+                {{ $posts->links() }}
+            </div>
+        @endsection
     </body>
 </html>
